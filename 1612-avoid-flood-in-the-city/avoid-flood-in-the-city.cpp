c42fed3010cd267e360e598f25a46a86a8f98da5
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2025-10-07 12:07:49 +0530

    Time: 214 ms (14.69%) | Memory: 103.6 MB (91.81%) - LeetSync

---
+++ 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<int> avoidFlood(vector<int>& rains) {
+        int n = rains.size();
+        vector<int> ans(n,1);
+        unordered_set<int> st;
+        vector<int> v;
+        queue<int> qc;
+        unordered_map<int, int> mp;
+        for(int i=0;i<n;i++){
+            if(rains[i]>0){
+                qc.push(rains[i]);
+                if(!mp.count(rains[i])){
+                    ans[i]=-1;
+                    mp[rains[i]]=i;
+                }else{
+                    int k = mp[rains[i]];
+                    auto it = upper_bound(v.begin(), v.end(), k);
+                    if(it!=v.end()){
+                        int ind = 0;
+                        ind = *it;
+                        if(ind<i){
+                            ans[ind]=rains[i];
+                            ans[i]=-1;
+                            mp[rains[i]]=i;
+                            v.erase(it);
+                        }else{
+                            vector<int> a;
+                            return a;
+                        }
+                    }else{
+                        vector<int> a;
+                        return a;
+                    }
+                }
+                
+            }else if(rains[i]==0){
+                if(!qc.empty()){
+                    v.push_back(i);
+                }
+            }
+        }
+        for(int i=0;i<v.size();i++){
+            ans[v[i]] = 1;
+        }
+        
+        return ans;
+    }
+};
