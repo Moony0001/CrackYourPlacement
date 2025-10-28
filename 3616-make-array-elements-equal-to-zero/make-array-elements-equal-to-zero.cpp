ab45aa4a2ade2bd76ac2a9b4831a6e9b7dab0881
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2025-10-28 19:54:47 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (80.73%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
@@ -1,34 +1,15 @@
 class Solution {
 public:
     int countValidSelections(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> pre (n,0);
-        vector<int> suf (n,0);
-        pre[0]=nums[0];
-        for(int i=1;i<n;i++){
-            pre[i] = nums[i]+pre[i-1];
+        int len = nums.size(), count = 0, left = 0, right = 0;
+        for (int v : nums) right += v;
+        for (int i = 0; i < len; i++) {
+            left += nums[i];
+            right -= nums[i];
+            if (nums[i] != 0) continue;
+            if (left == right) count += 2;
+            if (abs(left - right) == 1) count++;
         }
-        suf[n-1] = nums[n-1];
-        for(int i=n-2;i>=0;i--){
-            suf[i] = suf[i+1]+nums[i];
-        }
-
-        if(suf[0]==0){
-            int x = 2*n;
-            return x;
-        }
-
-        int ans = 0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                if(pre[i]==suf[i]){
-                    ans+=2;
-                }else if(abs(pre[i]-suf[i])==1){
-                    ans+=1;
-                }
-            }
-            
-        }
-        return ans;
+        return count;
     }
 };
