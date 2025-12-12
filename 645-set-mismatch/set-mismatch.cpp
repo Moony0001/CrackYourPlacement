5de10f4a00912a6872da20c0e10e8601bbb9c171
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2025-12-12 18:07:42 +0530

    Time: 0 ms (100.00%) | Memory: 25 MB (97.84%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -2,18 +2,22 @@ class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         vector<int> ans(2);
-        int n = nums.size();
-        vector<int> v(n);
-        iota(v.begin(), v.end(), 1);
-        unordered_set<int> st(v.begin(), v.end());
+        long long n = nums.size();
+        int cntl = 0;
+        long long cntsq = 0;
         for(int i=0;i<n;i++){
-            if(st.count(nums[i])){
-                st.erase(nums[i]);
-            }else{
-                ans[0] = nums[i];
-            }
+            cntl+=nums[i];
+            cntsq+=(nums[i]*nums[i]);
         }
-        ans[1] = *st.begin();
+        int suml = n*(n+1)/2;
+        long long sumsq = n*(2*n+1)*(n+1)/6;
+        int amb = suml-cntl;
+        int asmbs = sumsq-cntsq;
+        int apb = asmbs/amb;
+        int a = (amb+apb)/2;
+        int b = apb-a;
+        ans[0] = b;
+        ans[1] = a;
         return ans;
     }
 };
