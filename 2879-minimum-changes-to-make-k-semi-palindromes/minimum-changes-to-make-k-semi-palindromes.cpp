6d0c75e7f74f14b41fa5d8f0c7dc194eefc39d05
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2026-01-24 18:01:39 +0530

    Time: 191 ms (62.96%) | Memory: 14.5 MB (92.59%) - LeetSync

--- 2879-minimum-changes-to-make-k-semi-palindromes/minimum-changes-to-make-k-semi-palindromes.cpp
+++ 2879-minimum-changes-to-make-k-semi-palindromes/minimum-changes-to-make-k-semi-palindromes.cpp
@@ -1,5 +1,13 @@
 class Solution {
 public:
+    /*
+    This function checks the substring of s at the bounds of left and right with a jump
+    of d. We use two pointers, p1 and p2, send p2 to the back of the substring and
+    then use these two pointers to check how many changes are required to make the 
+    substring a semi palindrome by counting how many times there is a mismatch while
+    checking whether the substring is a palindrome or not
+    */
+
     int getchangeford(string& s, int left, int right, int d) {
         int changes = 0;
         for (int start = 0; start < d; start++) {
@@ -21,7 +29,13 @@ public:
 
     int minimumChanges(string s, int k) {
         int n = s.size();
-
+        /*
+        In the following code block we calculate the cost/no. of letters to be
+        changed to make the string s[i...j] a semi-palindrome. To do that we will
+        use two pointers and calculate the length using this and then take the
+        divisors of this len and use the function above to calculate the num of
+        changes required to make it a semi palindrome.
+        */
         vector<vector<int>> cost(n, vector<int>(n, INT_MAX / 2));
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
@@ -46,7 +60,14 @@ public:
             */
             dp[1][i] = cost[0][i];
         }
-
+        /*
+        We calculated the base case of groups = 1 above, now for groups = 2 till 
+        k (that we want), we use DP.
+        To do this, we use the bottom-up approach i.e. start small go big.
+        For each group size, for each length of string from 0 to n, we try
+        every index j<i as the end of the last group and take j+1 to i as the next 
+        group.        
+        */
         for (int groups = 2; groups <= k; groups++) {
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < i; j++) {
