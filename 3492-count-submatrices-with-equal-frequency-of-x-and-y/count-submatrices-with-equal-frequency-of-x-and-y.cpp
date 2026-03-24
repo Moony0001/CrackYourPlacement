2f975f686fcd5b915c3bcee99a090db89bf1a18a
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2026-03-24 17:12:29 +0530

    Time: 26 ms (76.22%) | Memory: 101 MB (86.38%) - LeetSync

--- 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.cpp
+++ 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     int numberOfSubmatrices(vector<vector<char>>& grid) {
-        int n = grid.size(), m = grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<int> countx(n, 0);
+        vector<int> county(n, 0);
         int ans = 0;
-        vector<vector<array<int, 2>>> sum(n + 1, vector<array<int, 2>>(m + 1));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 'X') {
-                    sum[i + 1][j + 1][0] =
-                        sum[i + 1][j][0] + sum[i][j + 1][0] - sum[i][j][0] + 1;
-                    sum[i + 1][j + 1][1] = 1;
-                } else if (grid[i][j] == 'Y') {
-                    sum[i + 1][j + 1][0] =
-                        sum[i + 1][j][0] + sum[i][j + 1][0] - sum[i][j][0] - 1;
-                    sum[i + 1][j + 1][1] = sum[i + 1][j][1] | sum[i][j + 1][1];
-                } else {
-                    sum[i + 1][j + 1][0] =
-                        sum[i + 1][j][0] + sum[i][j + 1][0] - sum[i][j][0];
-                    sum[i + 1][j + 1][1] = sum[i + 1][j][1] | sum[i][j + 1][1];
+        for(int i=0;i<m;i++){
+            int rowx = 0;
+            int rowy = 0;
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='X'){
+                    rowx++;
                 }
-                ans += (!sum[i + 1][j + 1][0] && sum[i + 1][j + 1][1]) ? 1 : 0;
+                if(grid[i][j]=='Y'){
+                    rowy++;
+                }
+                countx[j] += rowx;
+                county[j] += rowy;
+                
+                if(countx[j]!=0 && countx[j]==county[j]){
+                    ans++;
+                }
             }
         }
         return ans;
