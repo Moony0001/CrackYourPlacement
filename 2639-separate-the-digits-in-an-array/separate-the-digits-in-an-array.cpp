9659febf16f1a177d547a55a50c492341a69daca
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2026-05-11 12:33:47 +0530

    Time: 3 ms (57.62%) | Memory: 15.1 MB (81.79%) - LeetSync

--- 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.cpp
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
-    void digitize(int i, vector<int>& a){
-        int temp = i;
-        stack<int> st;
-        while(temp>0){
-            st.push(temp%10);
-            temp/=10;
-        }
-        while(!st.empty()){
-            a.push_back(st.top());
-            st.pop();
-        }
-
-    }
 
     vector<int> separateDigits(vector<int>& nums) {
         vector<int> ans;
         int n = nums.size();
         for(int i=0;i<n;i++){
-            digitize(nums[i], ans);
+            string s = to_string(nums[i]);
+            for(int j=0;j<s.size();j++){
+                ans.push_back(s[j]-'0');
+            }
         }
         return ans;
     }
