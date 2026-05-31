2f26f47bb4e26091830303553567d1a31bdaf868
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2026-05-31 12:48:43 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (22.19%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -2,19 +2,23 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n+1,vector<int>(2,0));
-        dp[0][0] = 1;
-        dp[0][1] = 1;
+        // vector<vector<int>> dp(n+1,vector<int>(2,0));
+        int pmini = 1;
+        int pmaxi = 1;
         int ans = INT_MIN;
+        int cmini = 0;
+        int cmaxi = 0;
         for(int i=0;i<n;i++){
             if(nums[i]>0){
-                dp[i+1][0] = min(nums[i], nums[i]*dp[i][0]);
-                dp[i+1][1] = max(nums[i], nums[i]*dp[i][1]);
+                cmini = min(nums[i], nums[i]*pmini);
+                cmaxi = max(nums[i], nums[i]*pmaxi);
             }else{
-                dp[i+1][0] = min(nums[i], nums[i]*dp[i][1]);
-                dp[i+1][1] = max(nums[i], nums[i]*dp[i][0]);
+                cmini = min(nums[i], nums[i]*pmaxi);
+                cmaxi = max(nums[i], nums[i]*pmini);
             }
-            ans = max(ans, dp[i+1][1]);
+            ans = max(ans, cmaxi);
+            pmini = cmini;
+            pmaxi = cmaxi;
         }
         return ans;
     }
