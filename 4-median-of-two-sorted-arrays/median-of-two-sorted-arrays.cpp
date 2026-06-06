bd3a63abfbdbe98a6e5ff27245f0843fd939cc2a
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2026-06-06 15:47:08 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (60.58%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,49 +1,37 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& A, vector<int>& B) {
-        int na = int(A.size()), nb = int(B.size());
-        int n = na + nb;
-        if (n % 2) {
-            return solve(A, B, n / 2, 0, na - 1, 0, nb - 1);
-        } else {
-            return 1.0 *
-                   (solve(A, B, n / 2 - 1, 0, na - 1, 0, nb - 1) +
-                    solve(A, B, n / 2, 0, na - 1, 0, nb - 1)) /
-                   2;
-        }
-    }
-    int solve(vector<int>& A, vector<int>& B, int k, int aStart, int aEnd,
-              int bStart, int bEnd) {
-        // If the segment of on array is empty, it means we have passed all
-        // its element, just return the corresponding element in the other
-        // array.
-        if (aEnd < aStart) {
-            return B[k - aStart];
-        }
-        if (bEnd < bStart) {
-            return A[k - bStart];
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if (nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);
         }
 
-        // Get the middle indexes and middle values of A and B.
-        int aIndex = (aStart + aEnd) / 2, bIndex = (bStart + bEnd) / 2;
-        int aValue = A[aIndex], bValue = B[bIndex];
+        int m = nums1.size(), n = nums2.size();
+        int left = 0, right = m;
 
-        // If k is in the right half of A + B, remove the smaller left half.
-        if (aIndex + bIndex < k) {
-            if (aValue > bValue) {
-                return solve(A, B, k, aStart, aEnd, bIndex + 1, bEnd);
+        while (left <= right) {
+            int partitionA = (left + right) / 2;
+            int partitionB = (m + n + 1) / 2 - partitionA;
+
+            int maxLeftA = (partitionA == 0) ? INT_MIN : nums1[partitionA - 1];
+            int minRightA = (partitionA == m) ? INT_MAX : nums1[partitionA];
+            int maxLeftB = (partitionB == 0) ? INT_MIN : nums2[partitionB - 1];
+            int minRightB = (partitionB == n) ? INT_MAX : nums2[partitionB];
+
+            if (maxLeftA <= minRightB && maxLeftB <= minRightA) {
+                if ((m + n) % 2 == 0) {
+                    return (max(maxLeftA, maxLeftB) +
+                            min(minRightA, minRightB)) /
+                           2.0;
+                } else {
+                    return max(maxLeftA, maxLeftB);
+                }
+            } else if (maxLeftA > minRightB) {
+                right = partitionA - 1;
             } else {
-                return solve(A, B, k, aIndex + 1, aEnd, bStart, bEnd);
+                left = partitionA + 1;
             }
         }
-        // Otherwise, remove the larger right half.
-        else {
-            if (aValue > bValue) {
-                return solve(A, B, k, aStart, aIndex - 1, bStart, bEnd);
-            } else {
-                return solve(A, B, k, aStart, aEnd, bStart, bIndex - 1);
-            }
-        }
-        return -1;
+
+        return 0.0;
     }
 };
