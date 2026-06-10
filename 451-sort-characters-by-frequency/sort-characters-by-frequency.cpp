b7efae6d2af967a3a4253c5e75119b423b5d774f
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2026-06-10 14:02:53 +0530

    Time: 4 ms (38.45%) | Memory: 18.8 MB (5.00%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        vector<int> cnt (62,0);
-        for(char c : s){
-            if(65<=c && c<=90){
-                cnt[c-'A']++;
-            }else if(97<=c && c<=122){
-                cnt[c-'a'+26]++;
-            }else{
-                cnt[c-'0'+52]++;
+        int n = s.size();
+        vector<int> cnt(128, 0);
+        
+        // Step 1: Count frequencies
+        for(char c : s) {
+            cnt[c]++;
+        }
+        
+        // Step 2: Create buckets. Index = Frequency.
+        // We use a vector of strings because multiple characters might have the same frequency.
+        vector<string> buckets(n + 1, "");
+        for(int i = 0; i < 128; i++) {
+            if(cnt[i] > 0) {
+                // Append the character 'cnt[i]' times to the bucket string
+                buckets[cnt[i]].append(cnt[i], i);
             }
         }
-
-        priority_queue<pair<int,char>> pq;
-        for(int i=0;i<62;i++){
-            if(cnt[i]!=0){
-                if(i<26){
-                    pq.push({cnt[i],i+'A'});
-                }else if(i<52){
-                    pq.push({cnt[i],i+'a'-26});
-                }else{
-                    pq.push({cnt[i],i+'0'-52});
-                }
+        
+        // Step 3: Read buckets backward
+        string ans = "";
+        for(int i = n; i >= 1; i--) {
+            if(!buckets[i].empty()) {
+                ans += buckets[i];
             }
         }
-        string ans;
-
-        while(!pq.empty()){
-            auto p = pq.top();
-            pq.pop();
-            for(int i=0;i<p.first;i++){
-                ans+=p.second;
-            }
-        }
+        
         return ans;
     }
 };
