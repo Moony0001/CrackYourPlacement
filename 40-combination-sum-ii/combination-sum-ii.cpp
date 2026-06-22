998721f0a3c728b8ccc4daa6723cad6516b92eb1
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2026-06-22 22:25:21 +0530

    Time: 2 ms (44.13%) | Memory: 13.8 MB (98.56%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+    void subsumk(vector<vector<int>>& ans, vector<int>& can, vector<int>& nums, int sum, int i){
+        if(sum==0){
+            ans.push_back(can);
+            return;
+        }
+        if(i==nums.size() || sum<0){
+            return;
+        }
+        
+        can.push_back(nums[i]);
+        subsumk(ans, can, nums, sum-nums[i], i+1);
+        can.pop_back();
+        
+        while(i+1<nums.size() && nums[i+1]==nums[i]){
+            i++;
+        }
+        
+        subsumk(ans, can, nums, sum, i+1);
+
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> can;
+        sort(candidates.begin(), candidates.end());
+        subsumk(ans, can, candidates, target, 0);
+        return ans;
+
+    }
+};
