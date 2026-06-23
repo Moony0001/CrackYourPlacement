1bba317731f54decf0903dcf6416eff07a0cab30
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2026-06-23 14:41:04 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (61.98%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
-
-    void subsumk(vector<vector<int>>& ans, vector<int>& can, vector<int>& nums, int sum, int i){
-        if(sum==0){
-            ans.push_back(can);
-            return;
-        }
-        if(i==nums.size() || sum<0){
-            return;
-        }
-        
-        can.push_back(nums[i]);
-        subsumk(ans, can, nums, sum-nums[i], i+1);
-        can.pop_back();
-        //Once we have included a number x in our subsequence, we will not include any of its duplicates again to prevent repetitions
-        //So, we skip all the same numbers
-        while(i+1<nums.size() && nums[i+1]==nums[i]){
-            i++;
-        }
-        
-        subsumk(ans, can, nums, sum, i+1);
-
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> result;
+        vector<int> current;
+        backtrack(candidates, target, 0, current, result);
+        return result;
     }
 
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
-        vector<int> can;
-        sort(candidates.begin(), candidates.end());
-        subsumk(ans, can, candidates, target, 0);
-        return ans;
-
+private:
+    void backtrack(const vector<int>& candidates, int target, int start,
+                   vector<int>& current, vector<vector<int>>& result) {
+        if (target == 0) {
+            result.push_back(current);
+            return;
+        }
+        for (int i = start; i < candidates.size(); ++i) {
+            if (candidates[i] > target) break; // pruning
+            if (i > start && candidates[i] == candidates[i-1]) continue; // skip duplicates
+            current.push_back(candidates[i]);
+            backtrack(candidates, target - candidates[i], i + 1, current, result);
+            current.pop_back();
+        }
     }
 };
