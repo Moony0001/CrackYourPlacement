20f28d1dc81bfd98a7985a993b5661dbbe517d9f
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2026-06-23 16:36:00 +0530

    Time: 6 ms (5.65%) | Memory: 14.6 MB (5.54%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    void subsum(vector<vector<int>>& ans, vector<int> can, int i, int s, int sum){
+        if(sum==0 && s==0){
+            ans.push_back(can);
+            return;
+        }
+        if(s==0 || sum<0 || i>9) return;
+        can.push_back(i);
+        subsum(ans, can, i+1, s-1, sum-i);
+        can.pop_back();
+        subsum(ans, can, i+1, s, sum);
+    }
+
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans;
+        vector<int> can; 
+        subsum(ans, can, 1, k, n);
+        return ans;
+    }
+};
