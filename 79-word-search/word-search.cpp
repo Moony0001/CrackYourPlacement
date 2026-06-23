3ccbb2e727ca2323ef3dc42fdcc8e40c0b5a340d
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2026-06-23 20:48:04 +0530

    Time: 27 ms (97.79%) | Memory: 10.5 MB (65.47%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,42 +1,44 @@
 class Solution {
 public:
+    bool check(vector<vector<char>>& board, string& s, int i, int j, int ind) {
+        if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size())
+            return false;
+        if (s[ind] != board[i][j])
+            return false;
+        if (ind == s.size() - 1 && s[ind] == board[i][j])
+            return true;
+        board[i][j] = '*';
 
-    bool check(vector<vector<char>>& board, vector<vector<int>>& vis, string s, int i, int j, int ind){
-        if(s[ind]!=board[i][j]) return false;
-        if(ind==s.size()-1 && s[ind]==board[i][j]) return true;
-        vis[i][j] = 1;
-        // bool a = false, b = false, c = false, d = false;
-        if(i+1<board.size() && !vis[i+1][j]){
-            if(check(board, vis, s, i+1, j, ind+1)){
+        int dir[5] = {-1, 0, 1, 0, -1};
+        for (int k = 0; k < 4; k++) {
+            if (check(board, s, i + dir[k], j + dir[k + 1], ind + 1)) {
                 return true;
             }
         }
-        if(i-1>=0 && !vis[i-1][j]){
-            if(check(board, vis, s, i-1, j, ind+1)){
-                return true;
-            }
-        }
-        if(j+1<board[0].size() && !vis[i][j+1]){
-            if(check(board, vis, s, i, j+1, ind+1)){
-                return true;
-            }
-        }
-        if(j-1>=0 && !vis[i][j-1]){
-            if(check(board, vis, s, i, j-1, ind+1)){
-                return true;
-            }
-        }
-        vis[i][j] = 0;
+        board[i][j] = s[ind];
         return false;
     }
 
     bool exist(vector<vector<char>>& board, string word) {
         int n = board.size();
         int m = board[0].size();
-        vector<vector<int>> vis(n, vector<int>(m,0));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(check(board, vis, word, i, j, 0)){
+
+        // Pruning to check whether board even has enough letters
+        vector<int> freq(128, 0);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                freq[board[i][j]]++;
+            }
+        }
+
+        for (char c : word) {
+            if (--freq[c] < 0)
+                return false;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (check(board, word, i, j, 0)) {
                     return true;
                 }
             }
