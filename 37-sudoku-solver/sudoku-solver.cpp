f1ded102f3b00c4597576b236c2a5072e22af9ae
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2026-06-24 20:51:34 +0530

    Time: 43 ms (97.64%) | Memory: 8.8 MB (70.94%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-    bool build(vector<vector<char>>& board, vector<unordered_set<int>>& rows, vector<unordered_set<int>>& col, vector<unordered_set<int>>& box, int i, int j){
+    bool build(vector<vector<char>>& board, int (&rows) [9], int (&col) [9], int (&box) [9], int i, int j){
         // If j ever becomes 9 then that means that the right boundary for that row was crossed so we need to actually move on to the first column of the next row
         if(j==9) return build(board, rows, col, box, i+1, 0);
         //If i ever becomes 9 this means that we crossed the last (8,8) square and it became i==9 this means we put all the numbers in the board and hence we can return true.
@@ -10,21 +10,22 @@ public:
         if(board[i][j]!='.') return build(board, rows, col, box, i, j+1);
 
         for(int k=1;k<=9;k++){
-            if(!rows[i].count(k) && !col[j].count(k) && !box[3*(i/3)+(j/3)].count(k)){
-                
+            int mask = 1<<k;
+            if(!(rows[i] & mask) && !(col[j] & mask) && !(box[3*(i/3)+(j/3)] & mask)){
+
                 board[i][j] = '0'+k;
-                rows[i].insert(k);
-                col[j].insert(k);
-                box[3*(i/3)+(j/3)].insert(k);
+                rows[i]|= 1<<k;
+                col[j]|= 1<<k;
+                box[3*(i/3)+(j/3)]|= 1<<k;
 
                 if(build(board, rows, col, box, i, j+1)){
                     return true;
                 }
                     
                 board[i][j] = '.';
-                rows[i].erase(k);
-                col[j].erase(k);
-                box[3*(i/3)+(j/3)].erase(k);
+                rows[i]^= mask;
+                col[j]^= mask;
+                box[3*(i/3)+(j/3)]^= mask;
                 
             }
         }
@@ -33,19 +34,17 @@ public:
     }
 
     void solveSudoku(vector<vector<char>>& board) {
-        vector<unordered_set<int>> rows (9);
-        vector<unordered_set<int>> col (9);
-        vector<unordered_set<int>> box (9);
+        int rows[9] = {0};
+        int col[9] = {0};
+        int box[9] = {0};
 
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
                 if(board[i][j]!='.'){
-                    rows[i].insert(board[i][j]-'0');
-                    col[j].insert(board[i][j]-'0');
-                    int indx = i/3;
-                    int indy = j/3;
-                    int ind = 3*indx+indy;
-                    box[ind].insert(board[i][j]-'0');
+                    int x = board[i][j]-'0';
+                    rows[i]|= 1<<x;
+                    col[j]|= 1<<x;
+                    box[3*(i/3)+(j/3)]|= 1<<x;
                 }
             }
         }
