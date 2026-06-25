70e57bf937a26d8e2aaa9d0e733aa4a4a1affa83
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2026-06-25 15:26:58 +0530

    Time: 29 ms (83.66%) | Memory: 41.1 MB (35.95%) - LeetSync

--- 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
+++ 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
@@ -11,7 +11,7 @@ public:
                 pre[i+1] = pre[i];
             }
         }
-
+        if(pre[n]==0) return 0;
         int ans = 0;
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
