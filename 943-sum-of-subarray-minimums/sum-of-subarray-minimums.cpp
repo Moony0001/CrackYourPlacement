484287f7b8e34d07d8fdd96e07c8ae815906bbc4
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2026-07-02 15:45:25 +0530

    Time: 48 ms (52.08%) | Memory: 178.1 MB (51.38%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -2,33 +2,25 @@ class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
         int n = arr.size();
-        vector<int> left (n,-1);
-        vector<int> right (n,n);
 
         stack<int> st;
-        for(int i=0;i<n;i++){
-            while(!st.empty() && arr[st.top()]>arr[i]){
-                right[st.top()] = i;
+        long long sum = 0;
+
+        for(int i=0;i<=n;i++){
+            while(!st.empty() && (i==n || arr[st.top()]>arr[i])){
+                int right = i;
+                int curr = st.top();
                 st.pop();
+                int left = st.empty() ? -1 : st.top();
+                int leftc = curr-left;
+                int rightc = right-curr;
+                long long cnt = leftc*rightc;
+                long long cont = (cnt*arr[curr])%1000000007;
+                sum = (sum+cont)%1000000007;
             }
             st.push(i);
         }
-        stack<int> st1;
-        for(int i=n-1;i>=0;i--){
-            while(!st1.empty() && arr[st1.top()]>=arr[i]){
-                left[st1.top()] = i;
-                st1.pop();
-            }
-            st1.push(i);
-        }
-        long long sum = 0;
-        for(int i=0;i<n;i++){
-            long long leftc = (i-left[i]);
-            long long rightc = (right[i]-i);
-            long long cnt = (leftc*rightc);
-            long long cont = (cnt*arr[i])%1000000007;
-            sum = (sum + cont)%1000000007;
-        }
+        
         return sum;
     }
 };
