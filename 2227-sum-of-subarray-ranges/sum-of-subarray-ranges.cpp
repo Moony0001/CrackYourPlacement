52a6816e65b630e788c0fe9f587cd7b93a96960e
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2026-07-02 17:31:35 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (74.30%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,17 +1,39 @@
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
-        long long sum = 0;
+        stack<int> inc;
+        stack<int> dec;
         int n = nums.size();
-        for(int i=0;i<n;i++){
-            int maxi = INT_MIN;
-            int mini = INT_MAX;
-            for(int j=i;j<n;j++){
-                maxi = max(maxi, nums[j]);
-                mini = min(mini, nums[j]);
-                sum = (sum+(maxi-mini));
+        long long minsum = 0;
+        long long maxsum = 0;
+        for(int i=0;i<=n;i++){
+            while(!inc.empty() && (i==n || nums[inc.top()]>nums[i])){
+                int right = i;
+                int curr = inc.top();
+                inc.pop();
+                int left = inc.empty() ? -1 : inc.top();
+                int leftc = curr-left;
+                int rightc = right-curr;
+                long long cnt = leftc*rightc;
+                long long cont = nums[curr]*cnt;
+                minsum += cont;
             }
+            inc.push(i);
+            while(!dec.empty() && (i==n || nums[dec.top()]<nums[i])){
+                int right = i;
+                int curr = dec.top();
+                dec.pop();
+                int left = dec.empty() ? -1 : dec.top();
+                int leftc = curr-left;
+                int rightc = right-curr;
+                long long cnt = leftc*rightc;
+                long long cont = nums[curr]*cnt;
+                maxsum+= cont;
+            }
+            dec.push(i);
         }
-        return sum;
+
+        long long ans = maxsum-minsum;
+        return ans;
     }
 };
