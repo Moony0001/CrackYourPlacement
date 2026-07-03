72d14d0a46c11f65e67413039271a404c2f77f28
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2026-07-03 16:47:51 +0530

    Time: 26 ms (77.22%) | Memory: 94.1 MB (8.91%) - LeetSync

--- 937-online-stock-span/online-stock-span.cpp
+++ 937-online-stock-span/online-stock-span.cpp
@@ -12,9 +12,9 @@ public:
         int ans = 1;
         while(!st.empty() && v[st.top()]<=price){
             st.pop();
-            int left = st.empty() ? -1 : st.top();
-            ans = max(ans, cnt-left);
         }
+        int left = st.empty() ? -1 : st.top();
+        ans = max(ans, cnt-left);
         st.push(cnt);
         v.push_back(price);
         cnt++;
