e897ff592767e4c0c34bf49eb0d4f419de2d6e87
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2026-07-03 14:43:26 +0530

    Time: 1 ms (95.01%) | Memory: 20.1 MB (46.41%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,104 +1,40 @@
 class Solution {
 public:
-    
-    int largestArea(vector<int>& histogram){
-        int n=histogram.size(), area=0;
-        stack<int> s;
-        
-        for(int i=0; i<n; i++){
-            while(!s.empty() && histogram[s.top()]>=histogram[i]){
-                int top = s.top();
-                s.pop();
-                
-                int start;
-                if(s.empty())
-                    start = -1;
-                else
-                    start = s.top();
-                    
-                
-                int curr_area = histogram[top] * (i - start -1);
-                area = max(area, curr_area);
+    int largestrect(vector<int> nums){
+        int n = nums.size();
+        stack<int> st;
+        long long ans = INT_MIN;
+        for(int i=0;i<=n;i++){
+            while(!st.empty() && (i==n || nums[st.top()]>nums[i])){
+                int curr = st.top();
+                st.pop();
+                int right = i;
+                int left = st.empty() ? -1 : st.top();
+                long long cnt = right-left-1;
+                long long cont = cnt*nums[curr];
+                ans = max(ans, cont);
             }
-            s.push(i);
+            st.push(i);
         }
-        
-        while(!s.empty()){
-            int top = s.top();
-            s.pop();
-
-            int start;
-            if(s.empty())
-                start = -1;
-            else
-                start = s.top();
-            
-            int curr_area = histogram[top] * (n - start -1);
-            area = max(area, curr_area);
-        }
-        
-        return area;
+        return ans;
     }
-    
+
+
     int maximalRectangle(vector<vector<char>>& matrix) {
-        int m=matrix.size();
-        if(m==0) return 0;
-        int n=matrix[0].size(), result=0;
-        vector<int> histogram(n, 0);
-        
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(matrix[i][j]=='1')
-                    histogram[j]+=1;
-                else
-                    histogram[j]=0;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int> h(m,0);
+        int ans = INT_MIN;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]=='1'){
+                    h[j] += 1;
+                }else{
+                    h[j] = 0;
+                }
             }
-            
-            result = max(result, largestArea(histogram));
-            cout<<result<<" ";
+            ans = max(largestrect(h), ans);
         }
-        return result;
+        return ans;
     }
 };
-
-/*
-
-2 0 2 1 1 
-
-3 1 3 2 2 
-
-
-Algorithm Outline:
-largestArea()
-
-        |
-    |   |       |
-|   |   |   |   |
-
-1   2   3   1   2   -> 5
-0   1   2   3   4   -> index
-
-1. stack
-2. iterate, check when value is less that top
-3. While less than top, area = height of bar * distance from i (anchor)
-4. Add to stack
-5. Check while stack not empty
-6. Return max_area
-
-main()
-1. Calculate histogra array for each row
-2. Check largest area in that row
-3. Return ans
-
-
-Horizontal - can check length
-Verticle - Keep height of each column top to bottum
-
-calculate area for each histogram in each row 
-
-1 0 1 0 0   -> 1
-1 0 1 1 1   -> 3
-1 1 1 1 1   -> 6
-1 0 1 1 1
-
-*/
