822a3121be989ff13d5310342c0bebab27f633d9
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2026-07-05 18:10:00 +0530

    Time: 1 ms (99.00%) | Memory: 42.6 MB (99.38%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -1,31 +1,40 @@
 class Solution {
 public:
-    int atmost(vector<int>& nums, int k){
-        int i = 0;
-        int n = nums.size();
-        vector<int> hash(n+1, 0);
-        int cnt = 0;
-        int ans = 0;
-        for(int j=0;j<nums.size();j++){
-            if(hash[nums[j]]==0){
-                cnt++;
-            }
-            hash[nums[j]]++;
-
-            while(cnt>k){
-                hash[nums[i]]--;
-                if(hash[nums[i]]==0){
-                    cnt--;
-                }
-                i++;
-            }
-
-            ans += (j-i+1);
-        }
-        return ans;
-    }
-
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        return atmost(nums, k) - atmost(nums, k-1);
+        int n = nums.size();
+        vector<int> freq(n + 1, 0);
+        int left = 0;
+        int distinct = 0;
+        int prefix_redundant = 0;
+        int ans = 0;
+
+        for (int right = 0; right < n; right++) {
+            if (freq[nums[right]] == 0) {
+                distinct++;
+            }
+            freq[nums[right]]++;
+
+            // 1. If we exceed k, shrink until valid and RESET our prefix counter!
+            if (distinct > k) {
+                freq[nums[left]]--;
+                left++;
+                distinct--;
+                prefix_redundant = 0;
+            }
+
+            // 2. Strip all redundant elements from the left of our valid window
+            while (freq[nums[left]] > 1) {
+                freq[nums[left]]--;
+                left++;
+                prefix_redundant++;
+            }
+
+            // 3. If window has exactly k distinct numbers, add (1 + redundant choices)
+            if (distinct == k) {
+                ans += (1 + prefix_redundant);
+            }
+        }
+
+        return ans;
     }
 };
