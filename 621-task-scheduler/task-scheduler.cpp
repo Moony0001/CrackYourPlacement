0ea3df5a3e787605342f1fd3b8d4727579c1b97b
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2026-07-07 15:13:58 +0530

    Time: 0 ms (100.00%) | Memory: 38.2 MB (84.04%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -2,41 +2,20 @@ class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
         vector<int> cnt (26,0);
+        int maxi = INT_MIN;
         for(char c : tasks){
             cnt[c-'A']++;
+            maxi = max(maxi, cnt[c-'A']);
         }
 
-        priority_queue<int> pq;
+        int cmax = 0;
         for(int i=0;i<26;i++){
-            if(cnt[i]>0){
-                pq.push(cnt[i]);
+            if(cnt[i]==maxi){
+                cmax++;
             }
         }
-        int ans = 0;
-        while(!pq.empty()){
-            int count = 0;
-            vector<int> wait;
-            for(int i=0;i<n+1;i++){
-                if(!pq.empty()){
-                    int temp = pq.top();
-                    pq.pop();
-                    count++;
-                    temp--;
-                    if(temp>0){
-                        wait.push_back(temp);
-                    }
-                }
-            }
-            for(int i=0;i<wait.size();i++){
-                pq.push(wait[i]);
-            }
-            if(pq.empty()){
-                ans+=count;
-            }else{
-                ans+=(n+1);
-            }
-            
-        }
+        int temp = (n+1)*(maxi-1) + cmax;
+        int ans = max((int)tasks.size(), temp);
         
         return ans;
     }
