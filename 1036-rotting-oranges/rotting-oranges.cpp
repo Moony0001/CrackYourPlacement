0037543c4b143a6bcd4c95bb545590673761f7a0
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2026-07-27 18:49:03 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (11.24%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -3,38 +3,46 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int fo = 0;
         queue<pair<int,int>> q;
+        vector<vector<int>> vis(n, vector<int>(m,0));
+
+        int cnt = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
                     q.push({i,j});
+                    vis[i][j] = 1;
                 }else if(grid[i][j]==1){
-                    fo++;
+                    cnt++;
+                }else{
+                    vis[i][j] = 1;
                 }
             }
         }
-        int time = 0;
-
-        vector<pair<int,int>> dirs = {{0,1},{1,0},{-1,0},{0,-1}};
-        while(fo!=0 && !q.empty()){
-            int qs = q.size();
-            for(int i=0;i<qs;i++){
-                int ri = q.front().first;
-                int rj = q.front().second;
+        int ans = 0;
+        vector<int> dx = {0, 1, 0, -1};
+        vector<int> dy = {1, 0, -1, 0};
+        while(!q.empty()){
+            int s = q.size();
+            ans++;
+            for(int i=0;i<s;i++){
+                int x = q.front().first;
+                int y = q.front().second;
                 q.pop();
-                for(auto d: dirs){
-                    int newX = ri+d.first;
-                    int newY = rj+d.second;
-                    if(newX>=0 && newX<n && newY>=0 && newY<m && grid[newX][newY]==1){
-                        grid[newX][newY] = 2;
-                        fo--;
-                        q.push({newX,newY});
+                for(int j=0;j<4;j++){
+                    int tempx = x+dx[j];
+                    int tempy = y+dy[j];
+                    if(tempx>=0 && tempx<n && tempy>=0 && tempy<m){
+                        if(!vis[tempx][tempy]){
+                            q.push({tempx, tempy});
+                            vis[tempx][tempy] = 1;
+                            cnt--;
+                        }
                     }
                 }
             }
-            time++;
         }
-        return fo==0 ? time : -1;
+        if(cnt>0) return -1;
+        return ans > 0 ? ans-1: 0;
     }
 };
