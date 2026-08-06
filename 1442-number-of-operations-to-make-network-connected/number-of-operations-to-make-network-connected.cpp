c020892a7fdc543166c1a8338be87dddd6dfed5d
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2026-08-06 16:30:52 +0530

    Time: 10 ms (64.20%) | Memory: 43.7 MB (65.17%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int find(int i, vector<int>& parent){
+        if(i==parent[i]) return i;
+
+        return parent[i] = find(parent[i], parent);
+    }
+
+    void unionf(int u, int v, vector<int>& parent, vector<int>& rank){
+        int u_parent = find(u, parent);
+        int v_parent = find(v, parent);
+
+        if(u_parent==v_parent) return;
+
+        if(rank[u_parent] > rank[v_parent]){
+            parent[v_parent] = u_parent;
+        }else if(rank[u_parent] < rank[v_parent]){
+            parent[u_parent] = v_parent;
+        }else{
+            parent[u_parent] = v_parent;
+            rank[v_parent]++;
+        }
+    }
+
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        int s = connections.size();
+        if(s<(n-1)) return -1;
+
+        vector<int> rank(n, 0);
+        vector<int> parent (n);
+        for(int i=0;i<n;i++){
+            parent[i] = i;
+        }
+
+        for(int i=0;i<s;i++){
+            int u = connections[i][0];
+            int v = connections[i][1];
+            unionf(u,v,parent, rank);
+        }
+
+        unordered_set<int> st;
+        for(int i=0;i<n;i++){
+            int x = find(i, parent);
+            if(!st.count(x)){
+                st.insert(x);
+            }
+        }
+        int ans = st.size()-1;
+        return ans;
+    }
+};
