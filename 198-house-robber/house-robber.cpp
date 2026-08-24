623bf25370b36485cbb079eab46a16dc7e31247e
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2026-08-24 11:45:59 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (90.43%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> memo(n+1, -1);
 
-        memo[0] = 0;
-        memo[1] = nums[0];
+        int prev1 = 0;
+        int prev2 = nums[0];
+        int ans = prev2;
 
         for(int i=2;i<=n;i++){
-            memo[i] = max(nums[i-1]+memo[i-2], memo[i-1]);
+            ans = max(nums[i-1]+prev1, prev2);
+            prev1 = prev2;
+            prev2 = ans;
         }
-        return memo[n];
+        return ans;
     }
 };
