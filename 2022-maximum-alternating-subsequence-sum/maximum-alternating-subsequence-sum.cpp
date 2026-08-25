73a9274aa86413ad271e6c0b1defd7c9bf184295
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2026-08-25 16:00:48 +0530

    Time: 101 ms (98.71%) | Memory: 94.7 MB (95.64%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -2,13 +2,18 @@ class Solution {
 public:
     long long maxAlternatingSum(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<long long>> dp (n+1, vector<long long>(2, 0));
+        // vector<vector<long long>> dp (n+1, vector<long long>(2, 0));
+        long long preveven = 0;
+        long long prevodd = 0;
 
         for(int i=1;i<=n;i++){
-            dp[i][0] = max(dp[i-1][1]+nums[i-1], dp[i-1][0]);
-            dp[i][1] = max(dp[i-1][0]-nums[i-1], dp[i-1][1]);
+            long long tempodd = max(preveven+nums[i-1], prevodd);
+            long long tempeven = max(prevodd-nums[i-1], preveven);
+
+            prevodd = tempodd;
+            preveven = tempeven;
         }
 
-        return max(dp[n][0], dp[n][1]);
+        return max(preveven, prevodd);
     }
 };
