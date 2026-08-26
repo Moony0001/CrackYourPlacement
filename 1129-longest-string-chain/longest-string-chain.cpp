1107c60dc1f96d4ff157449f084461d0045417ac
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2026-08-26 14:57:39 +0530

    Time: 1905 ms (7.10%) | Memory: 640.4 MB (16.87%) - LeetSync

---
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool help(string a, string b){
+        int cnt = 0;
+        int n = a.size();
+        int m = b.size();
+        if(n+1!=m) return false;
+        int i=0, j=0;
+        while(i<n && j<m){
+            if(a[i]==b[j]){
+                i++;
+                j++;
+            }else{
+                cnt++;
+                j++;
+            }
+        }
+        return (cnt<=1);
+    }
+
+    int longestStrChain(vector<string>& words) {
+        int n = words.size();
+        vector<int> dp(n, 1);
+        sort(words.begin(), words.end(), [](string& a, string& b){
+            return a.size() < b.size();
+        });
+        int ans = 1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(help(words[j], words[i])){
+                    dp[i] = max(dp[i], dp[j]+1);
+                }
+                ans = max(ans, dp[i]);
+            }
+        }
+        return ans;
+    }
+};
