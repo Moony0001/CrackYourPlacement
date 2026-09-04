94de838385f044fa31291f1e1cf2f09d3a2a09b9
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2026-09-04 19:16:06 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (85.39%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,46 +1,37 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        int lim = n/3;
-        int candidate1 = nums[0];
-        int cnt1 = 1;
-        int candidate2 = 1e9+1;
-        int cnt2 = 0;
-        for(int i=1;i<n;i++){
-            if(nums[i]!=candidate2 && !cnt1){
-                candidate1 = nums[i];
-                cnt1++;
-            }else if(nums[i]!=candidate1 && !cnt2){
-                candidate2 = nums[i];
-                cnt2++;
-            }else if(nums[i]==candidate1){
-                cnt1++;
-            }else if(nums[i]==candidate2){
-                cnt2++;
-            }else if(nums[i]!=candidate1 && nums[i]!=candidate2){
-                cnt1--;
-                cnt2--;
-            }
-        }
-        vector<int> ans;
-        cnt1 = 0;
-        cnt2 = 0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==candidate1){
-                cnt1++;
-            }
-            if(nums[i]==candidate2){
-                cnt2++;
+        int n = nums.size()/3;
+        int cand1 = 0, cand2 = 1, cnt1 = 0, cnt2 = 0;
+
+        for(int i : nums){
+            if(cnt1 > 0 && i==cand1) cnt1++;
+            else if(cnt2 > 0 && i==cand2) cnt2++;
+            else if(cnt1==0){
+                cand1 = i;
+                cnt1 = 1;
+            }else if(cnt2==0){
+                cand2 = i;
+                cnt2 = 1;
+            }else{
+                cnt1--; cnt2--;
             }
         }
 
-        if(cnt1>(n/3)){
-            ans.push_back(candidate1);
+        cnt1 = cnt2 = 0;
+        for(int i : nums){
+            if(i==cand1) cnt1++;
+            else if(i==cand2) cnt2++;
         }
-        if(cnt2>(n/3)){
-            ans.push_back(candidate2);
+
+        if(cnt1 > n && cnt2 > n){
+            return {cand1, cand2};
+        }else if(cnt1 > n){
+            return {cand1};
+        }else if(cnt2 > n){
+            return {cand2};
         }
-        return ans;
+
+        return {};
     }
 };
