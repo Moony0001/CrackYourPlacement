5c7fc4f2d9af54d03e4c90204e223007979159de
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2026-09-05 12:52:21 +0530

    Time: 3 ms (89.09%) | Memory: 23.9 MB (32.59%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -3,31 +3,19 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& nums) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
-
-        int i=0;
-        int j=1;
-        int last = 0;
+        vector<vector<int>> ans;
+        int i=0, j=i+1;
         while(i<n && j<n){
-            if(max(nums[i][1], nums[j][0])==nums[i][1]){
-                nums[i][0] = min(nums[i][0], nums[j][0]);
-                nums[i][1] = max(nums[i][1], nums[j][1]);
+            if(nums[i][1] >= nums[j][0]){
+                nums[i][1] = max(nums[j][1], nums[i][1]);
                 j++;
             }else{
-                nums[last][0] = nums[i][0];
-                nums[last][1] = nums[i][1];
-                last++;
+                ans.push_back(nums[i]);
                 i = j;
                 j++;
-            }
+            }            
         }
-
-        nums[last][0] = nums[i][0];
-        nums[last][1] = nums[i][1];
-        last++;
-        i++;
-
-        vector<vector<int>> ans;
-        ans.assign(nums.begin(), nums.begin()+last);
+        ans.push_back(nums[i]);
         return ans;
     }
 };
