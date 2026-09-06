11d4be631846362420cbfadd800585cc145528b9
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2026-09-06 13:52:47 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (29.30%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    int findKthPositive(vector<int>& arr, int k) {
-        int left = 0;
-        int right = arr.size() - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            // Calculate how many numbers are missing BEFORE arr[mid]
-            int missing = arr[mid] - (mid + 1);
-
-            // If we are missing fewer than k numbers, move right
-            if (missing < k) {
-                left = mid + 1;
-            } 
-            // If we are missing k or more numbers, move left
-            else {
-                right = mid - 1;
+    int findKthPositive(vector<int>& nums, int k) {
+        int n = nums.size();
+        int left = 1;
+        int right = nums[n-1]+k;
+        int ans = -1;
+        
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            auto it = upper_bound(nums.begin(), nums.end(), mid);
+            int ind = 0;
+            ind = it - nums.begin();
+            int cnt = mid-ind;
+            if(cnt >= k){
+                ans = mid;
+                right = mid-1;
+            }else if(cnt < k){
+                left = mid+1;
             }
         }
-
-        // The math perfectly simplifies to left + k
-        return left + k;
+        return ans;
     }
 };
